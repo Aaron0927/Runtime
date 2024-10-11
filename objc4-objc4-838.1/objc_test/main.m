730b9a298e5f9aa37be421f7e996db2dacef3350
kim <xiao.kim@tradego8.com>
2024-10-11 17:04:01 +0800

    添加测试工程

---
+++ objc4-objc4-838.1/objc_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_test
+//
+//  Created by kim on 2024/10/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
