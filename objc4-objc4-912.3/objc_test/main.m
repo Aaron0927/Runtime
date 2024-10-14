6bb8e21954a9a835fa681c62635b777547af8c4a
Aaron <aaronxiao0927@gmail.com>
2024-10-14 23:11:16 +0800

    添加 macOS 14.4 可编译源码

---
+++ objc4-objc4-912.3/objc_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_test
+//
+//  Created by Aaron on 2024/10/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
